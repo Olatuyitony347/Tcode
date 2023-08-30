9918f8fef89e335f20e0ea3e4d75becda9980aee
Olatuyitony347 <86169011+Olatuyitony347@users.noreply.github.com>
2023-08-30 02:36:29 -0700

    Add files via upload

---
+++ text.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string fullname;
+    cout << "Type your full name: ";
+    getline (cin, fullname);
+    cout << "Your name is: " <<fullname;
+    return 0;
+}
